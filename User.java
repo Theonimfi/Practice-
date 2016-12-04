4770e6ff57014b5ab97142599c8324f2cf327e12
Theonimfi <theon.anog@gmail.com>
2016-12-04 16:09:06 +0200

    New programme

---
+++ User.java
@@ -0,0 +1,39 @@
+public class User {
+	private String username;
+	private String password;
+	private String fullname;
+
+	public User(String uname, String passwd, String fname) {
+	  username = uname;
+	  password = passwd;
+	  fullname = fname;
+	}
+
+	public void setUsername(String uname) {
+		username = uname;
+	}
+
+	public void setPassword(String passwd) {
+		 password = passwd;
+	}
+
+	public void setFullname(String fname) {
+		fullname = fname;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public String toString() {
+	  return "<" + username + ", " + password + ", " + fullname + ">";
+	}
+}
